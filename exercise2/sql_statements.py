ccf2dfed77e53462b6f7264c5e18401f4eac241b
vivian <viviankaun@gmail.com>
2021-12-05 17:13:55 -0800

    Add files via upload

---
+++ exercise2/sql_statements.py
@@ -0,0 +1,119 @@
+CREATE_TRIPS_TABLE_SQL = """
+CREATE TABLE IF NOT EXISTS trips (
+trip_id INTEGER NOT NULL,
+start_time TIMESTAMP NOT NULL,
+end_time TIMESTAMP NOT NULL,
+bikeid INTEGER NOT NULL,
+tripduration DECIMAL(16,2) NOT NULL,
+from_station_id INTEGER NOT NULL,
+from_station_name VARCHAR(100) NOT NULL,
+to_station_id INTEGER NOT NULL,
+to_station_name VARCHAR(100) NOT NULL,
+usertype VARCHAR(20),
+gender VARCHAR(6),
+birthyear INTEGER,
+PRIMARY KEY(trip_id))
+DISTSTYLE ALL;
+"""
+
+CREATE_STATIONS_TABLE_SQL = """
+CREATE TABLE IF NOT EXISTS stations (
+id INTEGER NOT NULL,
+name VARCHAR(250) NOT NULL,
+city VARCHAR(100) NOT NULL,
+latitude DECIMAL(9, 6) NOT NULL,
+longitude DECIMAL(9, 6) NOT NULL,
+dpcapacity INTEGER NOT NULL,
+online_date TIMESTAMP NOT NULL,
+PRIMARY KEY(id))
+DISTSTYLE ALL;
+"""
+
+COPY_SQL = """
+COPY {}
+FROM '{}'
+ACCESS_KEY_ID '{{}}'
+SECRET_ACCESS_KEY '{{}}'
+IGNOREHEADER 1
+DELIMITER ','
+"""
+
+COPY_MONTHLY_TRIPS_SQL = COPY_SQL.format(
+    "trips",
+    "s3://udacity-dend/data-pipelines/divvy/partitioned/{year}/{month}/divvy_trips.csv"
+)
+
+COPY_ALL_TRIPS_SQL = COPY_SQL.format(
+    "trips",
+    "s3://udacity-dend/data-pipelines/divvy/unpartitioned/divvy_trips_2018.csv"
+)
+
+COPY_STATIONS_SQL = COPY_SQL.format(
+    "stations",
+    "s3://udacity-dend/data-pipelines/divvy/unpartitioned/divvy_stations_2017.csv"
+)
+
+LOCATION_TRAFFIC_SQL = """
+BEGIN;
+DROP TABLE IF EXISTS station_traffic;
+CREATE TABLE station_traffic AS
+SELECT
+    DISTINCT(t.from_station_id) AS station_id,
+    t.from_station_name AS station_name,
+    num_departures,
+    num_arrivals
+FROM trips t
+JOIN (
+    SELECT
+        from_station_id,
+        COUNT(from_station_id) AS num_departures
+    FROM trips
+    GROUP BY from_station_id
+) AS fs ON t.from_station_id = fs.from_station_id
+JOIN (
+    SELECT
+        to_station_id,
+        COUNT(to_station_id) AS num_arrivals
+    FROM trips
+    GROUP BY to_station_id
+) AS ts ON t.from_station_id = ts.to_station_id
+"""
+ 
+CREATE_EVENT_TABLE_SQL =  """ CREATE TABLE IF NOT EXISTS stagingevents (
+                event_id    BIGINT IDENTITY(0,1)    NOT NULL,
+                artist      VARCHAR                 NULL,
+                auth        VARCHAR                 NULL,
+                firstName   VARCHAR                 NULL,
+                gender      VARCHAR                 NULL,
+                itemInSession VARCHAR               NULL,
+                lastName    VARCHAR                 NULL,
+                length      VARCHAR                 NULL,
+                level       VARCHAR                 NULL,
+                location    VARCHAR                 NULL,
+                method      VARCHAR                 NULL,
+                page        VARCHAR                 NULL,
+                registration VARCHAR                NULL,
+                sessionId   INTEGER                 NOT NULL SORTKEY,
+                song        VARCHAR                 NULL,
+                status      INTEGER                 NULL,
+                ts          BIGINT                  NOT NULL,
+                userAgent   VARCHAR                 NULL,
+                userId      INTEGER                 NULL  DISTKEY);""" 
+
+ 
+
+ 
+    
+COPY_event_SQL = ("""COPY stagingevents 
+    FROM 's3://udacity-dend/log_data' 
+    ACCESS_KEY_ID 'AKIAVRSUSCR2NN2FOXPP'
+    SECRET_ACCESS_KEY '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'
+    format as json  's3://udacity-dend/log_json_path.json' 
+    STATUPDATE ON
+    ;
+""" )
+
+ 
+
+ 
+
